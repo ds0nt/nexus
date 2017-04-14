b95dede02b27014c5b7004e7507bbdcf59d07310
Daniel Sont <dan.sont@gmail.com>
2017-04-13 20:39:21 -0400

    add mutex locking to close vs send operations on client

--- client.go
+++ client.go
@@ -2,21 +2,25 @@ package nexus
 
 import (
 	"context"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 type Client struct {
 	name        string
+	closed      bool
 	context     context.Context
 	cancel      context.CancelFunc
 	messageChan chan *Packet
 	Env         map[interface{}]interface{}
+	sendCloseMu sync.Mutex
 }
 
 func newClient(conn *websocket.Conn) *Client {
 	ctx, cancel := context.WithCancel(context.Background())
 	return &Client{
+		sendCloseMu: sync.Mutex{},
 		name:        conn.RemoteAddr().String(),
 		context:     ctx,
 		cancel:      cancel,
@@ -26,15 +30,23 @@ func newClient(conn *websocket.Conn) *Client {
 }
 
 func (c *Client) close() {
+	c.sendCloseMu.Lock()
+	defer c.sendCloseMu.Unlock()
+	c.closed = true
 	c.cancel()
 	close(c.messageChan)
 }
 
 func (c *Client) Send(p *Packet) {
-	select {
-	case <-c.context.Done():
-	case c.messageChan <- p:
+	if c.closed {
+		return
 	}
+	c.sendCloseMu.Lock()
+	defer c.sendCloseMu.Unlock()
+	if c.closed {
+		return
+	}
+	c.messageChan <- p
 }
 
 func (c *Client) String() string {
